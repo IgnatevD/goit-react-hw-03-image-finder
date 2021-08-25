215bfe37fa4d1d1c6eb416d9fffff2295cc84f88
IgnatevD <ignatevds@gmail.com>
2021-08-25 22:04:16 +0300

    Добавлены основные компоненты в прокте

---
+++ src/Components/App.jsx
@@ -0,0 +1,23 @@
+import ImageGallery from "./ImageGallery/ImageGallery"
+import React, {Component} from "react"
+import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem"
+import Searchbar from "./Searchbar/Searchbar"
+
+class App extends Component {
+    state = {  }
+    render() {
+        return (
+   <div>
+        <Searchbar> 
+        <ImageGallery>
+        <ImageGalleryItem>
+        <Loader>
+        <Button>
+        <Modal></Modal>
+   </div>
+        
+        );
+    }
+}
+
+export default App;
---
+++ src/Components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem"
+
+const ImageGallery = () => {
+    return (
+<ul className="ImageGallery">
+  <ImageGalleryItem/>
+</ul>
+    );
+}
+
+export default ImageGallery;
